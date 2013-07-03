852f9bbad955a221c4488c0167ad1b385d0bc767
Felix Bytow <felix.bytow@googlemail.com>
2013-07-03 19:04:48 +0200

    fixed tabs and spaces

--- bofh_main.c
+++ bofh_main.c
@@ -35,58 +35,58 @@ struct proc_dir_entry *bofh_proc_file;
 /* here the file /proc/{BOFH_FILENAME} is filled with content */
 static int bofh_content(struct seq_file *m, void *v)
 {
-	char const *excuse;
-	size_t len;
+    char const *excuse;
+    size_t len;
 
-	/* the index of the chosen excuse... gets a random value */
-	unsigned int index;
+    /* the index of the chosen excuse... gets a random value */
+    unsigned int index;
 
-	get_random_bytes(&index, sizeof(unsigned int));
-	index %= max_excuse;
+    get_random_bytes(&index, sizeof(unsigned int));
+    index %= max_excuse;
 
-	excuse = excuses[index];
-	len = strlen(excuse);
+    excuse = excuses[index];
+    len = strlen(excuse);
 
-	pr_alert("kmod_bofh: Return BOFH excuse #%d: %s\n", index, excuse);
-	seq_printf(m, "%s\n", excuse);
+    pr_alert("kmod_bofh: Return BOFH excuse #%d: %s\n", index, excuse);
+    seq_printf(m, "%s\n", excuse);
 
-	return 0;
+    return 0;
 }
 
 static int bofh_open(struct inode * inode, struct file * file)
 {
-	return single_open(file, &bofh_content, NULL);
+    return single_open(file, &bofh_content, NULL);
 }
 
 static int __init bofh_init(void)
 {
-	struct file_operations ops = {
-		.owner   = THIS_MODULE,
-		.open  	 = &bofh_open,
-    	.read    = &seq_read,
-    	.llseek	 = &seq_lseek,
-    	.release = &single_release,
-	};
+    struct file_operations ops = {
+        .owner   = THIS_MODULE,
+        .open    = &bofh_open,
+        .read    = &seq_read,
+        .llseek  = &seq_lseek,
+        .release = &single_release,
+    };
 
-	bofh_proc_file = proc_create(BOFH_FILENAME, 0644, NULL, &ops);
+    bofh_proc_file = proc_create(BOFH_FILENAME, 0644, NULL, &ops);
 
-	if (unlikely(bofh_proc_file == NULL)) {
-		pr_alert("Error: kmod_bofh could not initialize /proc/%s\n",
-			BOFH_FILENAME);
-		return -ENOMEM;
-	}
+    if (unlikely(bofh_proc_file == NULL)) {
+        pr_alert("Error: kmod_bofh could not initialize /proc/%s\n",
+            BOFH_FILENAME);
+        return -ENOMEM;
+    }
 
-	proc_set_user(bofh_proc_file, GLOBAL_ROOT_UID, GLOBAL_ROOT_GID);
-	proc_set_size(bofh_proc_file, 0);
+    proc_set_user(bofh_proc_file, GLOBAL_ROOT_UID, GLOBAL_ROOT_GID);
+    proc_set_size(bofh_proc_file, 0);
 
-	pr_debug("BOFH module inititialized!\n");
-	return 0;
+    pr_debug("BOFH module inititialized!\n");
+    return 0;
 }
 
 static void __exit bofh_exit(void)
 {
-	remove_proc_entry(BOFH_FILENAME, bofh_proc_file);
-	pr_debug("BOFH module uninitialized!\n");
+    remove_proc_entry(BOFH_FILENAME, bofh_proc_file);
+    pr_debug("BOFH module uninitialized!\n");
 }
 
 module_init(bofh_init);
